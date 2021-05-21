e0098868dcac618284516048476ac7b408d23a8c
Mason Snow <82908624+masonhsnow@users.noreply.github.com>
2021-05-20 22:06:45 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 //---------------------------------------------------------------------------------
-//Fighters object + powerlevel values generators
+//Fighters object w/ powerlevel values as RNG
 const fighters = {
   goku: Math.trunc(Math.random() * 80) + 30,
   vegeta: Math.trunc(Math.random() * 60) + 40,
@@ -13,15 +13,49 @@ const fighters = {
 //---------------------------------------------------------------------------------
 //Fighter names array from object
 const fighterNames = Object.keys(fighters);
-const fighterPowerLevel = Object.values(fighters);
+//---------------------------------------------------------------------------------
+
+//Other repeated DOM queries as variables
+const fighter1 = document.querySelector(`.fighter-1`);
+const fighter2 = document.querySelector(`.fighter-2`);
+const modalHeader = document.querySelector(`.modal-header`);
+
+//---------------------------------------------------------------------------------
+
+//Function to re-roll power level values for all fighters
+const reRollPowerLevel = function () {
+  fighters.goku = Math.trunc(Math.random() * 80) + 30;
+  fighters.vegeta = Math.trunc(Math.random() * 60) + 40;
+  fighters.piccolo = Math.trunc(Math.random() * 45) + 5;
+  fighters.gohan = Math.trunc(Math.random() * 70) + 30;
+  fighters.android18 = Math.trunc(Math.random() * 40) + 10;
+  fighters.krillen = Math.trunc(Math.random() * 35) + 5;
+  fighters.yamcha = Math.trunc(Math.random() * 30) + 1;
+};
+
 //---------------------------------------------------------------------------------
 
 //Function to remove all dynamic classes
 const removeDynamicClasses = function () {
-  document.querySelector(`.fighter-1`).classList.remove(`winner`);
-  document.querySelector(`.fighter-2`).classList.remove(`winner`);
-  document.querySelector(`.fighter-1`).classList.remove(`draw`);
-  document.querySelector(`.fighter-2`).classList.remove(`draw`);
+  fighter1.classList.remove(`winner`);
+  fighter2.classList.remove(`winner`);
+  fighter1.classList.remove(`draw`);
+  fighter2.classList.remove(`draw`);
+};
+//---------------------------------------------------------------------------------
+
+//Function to add hidden class to modal window + overlay
+const closeModalWindow = function () {
+  document.querySelector(`.modal`).classList.add(`hidden`);
+  document.querySelector(`.overlay`).classList.add(`hidden`);
+};
+
+//---------------------------------------------------------------------------------
+
+//Function to remove hidden close and display modal window + overlay
+const openModalWindow = function () {
+  document.querySelector(`.modal`).classList.remove(`hidden`);
+  document.querySelector(`.overlay`).classList.remove(`hidden`);
 };
 //---------------------------------------------------------------------------------
 
@@ -55,13 +89,7 @@ const generateFighters = function () {
 //Function to generate winner of 2 selected fighters
 const whoWins = function () {
   //regenerate fighter power levels on click
-  fighters.goku = Math.trunc(Math.random() * 80) + 30;
-  fighters.vegeta = Math.trunc(Math.random() * 60) + 40;
-  fighters.piccolo = Math.trunc(Math.random() * 45) + 5;
-  fighters.gohan = Math.trunc(Math.random() * 70) + 30;
-  fighters.android18 = Math.trunc(Math.random() * 40) + 10;
-  fighters.krillen = Math.trunc(Math.random() * 35) + 5;
-  fighters.yamcha = Math.trunc(Math.random() * 30) + 1;
+  reRollPowerLevel();
 
   //log the fighters to console
   console.log(fighter1Selection, fighter2Selection);
@@ -73,29 +101,20 @@ const whoWins = function () {
   //conditional statement adding `winner` class to the winner of the fight
   if (fighters[fighter1Selection] > fighters[fighter2Selection]) {
     console.log(`${fighter1Selection} is the winner!`);
-    document.querySelector(`.fighter-1`).classList.add(`winner`);
-    document.querySelector(`.modal`).classList.remove(`hidden`);
-    document.querySelector(`.overlay`).classList.remove(`hidden`);
-    document.querySelector(
-      `.modal-header`
-    ).innerHTML = `${fighter1Selection} is the winner!`;
-    // alert(`${fighter1Selection} is the winner!`);
+    fighter1.classList.add(`winner`);
+    openModalWindow();
+    modalHeader.innerHTML = `${fighter1Selection} is the winner!`;
   } else if (fighters[fighter1Selection] === fighters[fighter2Selection]) {
-    document.querySelector(`.fighter-1`).classList.add(`draw`);
-    document.querySelector(`.fighter-2`).classList.add(`draw`);
-    document.querySelector(`.modal`).classList.remove(`hidden`);
-    document.querySelector(`.overlay`).classList.remove(`hidden`);
-    document.querySelector(`.modal-header`).innerHTML = `It's a draw!`;
+    fighter1.classList.add(`draw`);
+    fighter2.classList.add(`draw`);
+    openModalWindow();
+    modalHeader.innerHTML = `It's a draw!`;
     console.log(`It's a draw!`);
   } else {
     console.log(`${fighter2Selection} is the winner!`);
-    document.querySelector(`.fighter-2`).classList.add(`winner`);
-    document.querySelector(`.modal`).classList.remove(`hidden`);
-    document.querySelector(`.overlay`).classList.remove(`hidden`);
-    document.querySelector(
-      `.modal-header`
-    ).innerHTML = `${fighter2Selection} is the winner!`;
-    // alert(`${fighter2Selection} is the winner!`);
+    fighter2.classList.add(`winner`);
+    openModalWindow();
+    modalHeader.innerHTML = `${fighter2Selection} is the winner!`;
   }
 };
 //---------------------------------------------------------------------------------
@@ -105,19 +124,28 @@ document
   .querySelector(`.btn-generate`)
   .addEventListener(`click`, generateFighters);
 
+//---------------------------------------------------------------------------------
+
 //Event listener for "fight" button
 document.querySelector(`.btn-fight`).addEventListener(`click`, whoWins);
 
+//---------------------------------------------------------------------------------
+
 //Event listener for "close model" x button on click
-document.querySelector(`.close-modal`).addEventListener(`click`, function () {
-  document.querySelector(`.modal`).classList.add(`hidden`);
-  document.querySelector(`.overlay`).classList.add(`hidden`);
-});
+document
+  .querySelector(`.close-modal`)
+  .addEventListener(`click`, closeModalWindow);
+
+//---------------------------------------------------------------------------------
 
 //Event listener for to close modal on keypress
 document.addEventListener(`keydown`, function (e) {
   if (e.key === `Escape`) {
-    document.querySelector(`.modal`).classList.add(`hidden`);
-    document.querySelector(`.overlay`).classList.add(`hidden`);
+    closeModalWindow();
   }
 });
+
+//---------------------------------------------------------------------------------
+
+//Event listener to close modal window on click of overlay
+document.querySelector(`.overlay`).addEventListener(`click`, closeModalWindow);
